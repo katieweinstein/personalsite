e361735a2ea1bb08c29dfedfa28a7b433b0e1a41
Katie Weinstein <katie.weinstein3@gmail.com>
2021-03-07 22:06:04 -0500

    change public path

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
     path: path.resolve(__dirname, 'public/'),
-    publicPath: '/public/',
+    publicPath: '/',
     filename: 'bundle.js',
   },
   devServer: {
