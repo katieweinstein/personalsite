32c3bc07ef8e1e9b679add5d3acc99f37271702d
Katie Weinstein <katie.weinstein3@gmail.com>
2021-03-08 14:53:49 -0500

    add publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
+    publicPath: '/dist/',
   },
   devServer: {
     contentBase: path.join(__dirname, 'public/'),
