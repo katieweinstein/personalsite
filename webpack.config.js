9e1ab0796940e19b7e8e28b9e9486b0af624e83e
Katie Weinstein <katie.weinstein3@gmail.com>
2021-03-07 22:13:45 -0500

    edit path

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, 'public/'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
   devServer: {
