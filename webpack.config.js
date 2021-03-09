c4ca316a60344d1a5a30c00e4402bb433347a435
Katie Weinstein <katie.weinstein3@gmail.com>
2021-03-08 20:50:04 -0500

    remove publicpath

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/',
   },
   devServer: {
     contentBase: path.join(__dirname, 'public/'),
