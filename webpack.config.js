dd19c7d04adb3b351374f1452eb62a13c6adfa8a
Katie Weinstein <katie.weinstein3@gmail.com>
2021-03-08 20:58:02 -0500

    remove dirname in output

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve('./dist'),
     filename: 'bundle.js',
   },
   devServer: {
