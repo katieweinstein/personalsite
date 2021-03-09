0964b0517ac6041a39238e80878be9b9809a6f5b
Katie Weinstein <katie.weinstein3@gmail.com>
2021-03-08 21:04:24 -0500

    change output path

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve('../dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   devServer: {
